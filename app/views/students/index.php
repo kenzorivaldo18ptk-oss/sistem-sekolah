b6f51899a16352b8b0bdff4432086608d594a77d
kenzorivaldo18ptk-oss <kenzorivaldo18ptk@gmail.com>
2026-03-10 08:32:23 +0700

    wip: implement student index page

--- app/views/students/index.php
+++ app/views/students/index.php
@@ -6,8 +6,34 @@
     <title>Daftar Siswa</title>
     <link rel="stylesheet" href="/css/output.css">
 </head>
-<body>
-    <h1 class="text-red-500">Daftar Siswa</h1>
-    <p>Menampilkan daftar siswa</p>
+<body class="min-h-screen flex flex-col bg-gray-100">
+    <header class="bg-blue-500 text-white">
+        <div class="flex items-center justify-between container mx-auto p-4">
+            <a href="/students" class="font-bold text-xl">Sistem Sekolah</a>
+            <a href="/students/create" class="bg-white text-blue-500 px-4 py-2 rounded-lg">+ Tambah Siswa</a>
+        </div>
+    </header>
+ 
+    <main class="grow container mx-auto">
+        <div class="mt-8">
+ 
+                <div class="bg-white shadow rounded-lg p-4">
+                    <h1 class="text-2xl font-bold">Daftar Siswa</h1>
+                    <p>Menampilkan daftar siswa yang terdaftar</p>
+                </div>
+ 
+ 
+                <div class="bg-white shadow rounded-lg">
+ 
+                </div>
+ 
+        </div>
+</main>
+ 
+    <footer class="bg-gray-800 text-white">
+        <div class="text-center p-4">
+            &copy <?= date('Y')?> - Sistem Sekolah SMK Kristen Immanuel
+        </div>
+    </footer>
 </body>
 </html>
