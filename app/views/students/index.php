0fe3fdc12142fdd74a4abecad66c467928dfaea0
kenzorivaldo18ptk-oss <kenzorivaldo18ptk@gmail.com>
2026-03-31 07:56:04 +0700

    feat: implement student index page

--- app/views/students/index.php
+++ app/views/students/index.php
@@ -15,7 +15,7 @@
     </header>
  
     <main class="grow container mx-auto">
-        <div class="mt-8">
+        <div class="mt-8 space-y-4">
  
                 <div class="bg-white shadow rounded-lg p-4">
                     <h1 class="text-2xl font-bold">Daftar Siswa</h1>
@@ -24,7 +24,34 @@
  
  
                 <div class="bg-white shadow rounded-lg">
- 
+                    <table class="w-full">
+                        <thead class="bg-gray-200">
+                            <tr>
+                                <th class="py-2 px-4 text-left">No</th>
+                                <th class="py-2 px-4 text-left">Nama</th>
+                                <th class="py-2 px-4 text-left">NIS</th>
+                                <th class="py-2 px-4 text-left">Kelas</th>
+                                <th class="py-2 px-4 text-left">No Telepon</th>
+                                <th class="py-2 px-4 ">Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="py-2 px-4 text-left">1</td>
+                                <td class="py-2 px-4 text-left">Andi</td>
+                                <td class="py-2 px-4 text-left">1234</td>
+                                <td class="py-2 px-4 text-left">11 TKJ 3</td>
+                                <td class="py-2 px-4 text-left">0812345678</td>
+                                <td class="py-2 px-4">
+                                    <div class="flex items-center justify-center gap-4">
+                                        <a href="" class="text-green-500">Detail</a>
+                                        <a href="" class="text-yellow-500">Edit</a>
+                                        <a href="" class="text-red-500">Hapus</a>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
  
         </div>
