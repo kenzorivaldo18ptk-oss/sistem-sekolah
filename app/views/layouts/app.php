143ff64054d9dfa3cbc4654c85bdf83842192ebe
kenzorivaldo18ptk-oss <kenzorivaldo18ptk@gmail.com>
2026-04-10 10:57:18 +0700

    feat: implement layout

--- app/views/layouts/app.php
+++ app/views/layouts/app.php
@@ -5,23 +5,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Daftar Siswa</title>
     <link rel="stylesheet" href="/css/output.css">
-</head>
+</head>        
+<!-- Header -->
 <body class="min-h-screen flex flex-col bg-gray-100">
-    <header class="bg-blue-500 text-white">
-        <div class="flex items-center justify-between container mx-auto p-4">
-            <a href="/students" class="font-bold text-xl">Sistem Sekolah</a>
-            <a href="/students/create" class="bg-white text-blue-500 px-4 py-2 rounded-lg">+ Tambah Siswa</a>
+    <?php
+    require_once '../app/views/layouts/partials/header.php'
+    ?>
+    <!--Main-->
+    <main class="bg-gray-100 grow container mx-auto ">
+            <!--Card Header-->
+            <div class="bg-white shadow rounded-lg p-4">
+                <h1 class="text-xl font-bold">Daftar Siswa</h1>
+                <p>Menampilkan daftar Siswa yang terdaftar</p>
+            </div>
+            <div class="bg-white shadow rounded-lg p-4">
+                <table class="w-full">
+                    <thead class="bg-gray-200">
+                        <tr>
+                            <th class="py-2 px-4 text-left">Nomor</th>
+                            <th class="py-2 px-4 text-left">Nama</th>
+                            <th class="py-2 px-4 text-left">Nis</th>
+                            <th class="py-2 px-4 text-left">Kelas</th>
+                            <th class="py-2 px-4 text-left">Nomor telepon</th>
+                            <th class="py-2 px-4 ">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="py-2 px-4 text-left">1</td>
+                            <td class="py-2 px-4 text-left">Pokemon</td>
+                            <td class="py-2 px-4 text-left">1234</td>
+                            <td class="py-2 px-4 text-left">XITKJ3</td>
+                            <td class="py-2 px-4 text-left">0812345678</td>
+                            <td class="py-2 px-4 text-left">
+                                <div class="flex items-center justify-center gap-4">
+                                            <a href="/students/1" class="text-green-500">Detail</a>
+                                            <a href="/students/1/edit" class="text-yellow-500">Edit</a>
+                                            <a href="" class="text-red-500">Hapus</a>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </header>
- 
-    <main class="grow container mx-auto">
-        <?php require_once $content ?>
     </main>
- 
-    <footer class="bg-gray-800 text-white">
-        <div class="text-center p-4">
-            &copy <?= date('Y')?> - Sistem Sekolah SMK Kristen Immanuel
-        </div>
-    </footer>
+<!--Card Header End-->
+<!--Card Content-->
+<div class="bg-white shadow rounded-lg">
+
+</div>
+    </main>
+<!-- Footer -->
+   <?php
+    require_once '../app/views/layouts/partials/footer.php'
+    ?>
+    <!-- The End Of Footer -->
+   
 </body>
 </html>
