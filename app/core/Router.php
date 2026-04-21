5cd942bdb4bca61773fc405bacc3b6aaf40f9221
kenzorivaldo18ptk-oss <kenzorivaldo18ptk@gmail.com>
2026-04-21 08:19:00 +0700

    feat: implement create page

--- app/core/Router.php
+++ app/core/Router.php
@@ -1,11 +1,11 @@
 <?php
 namespace App\Core;
-use App\Controllers\StudentController;
+ 
 class Router
 {
-    private array $routes =[];
+    private array $routes = [];
  
-    public function add(string $method, string $uri, string $controller, string $function)
+    public function add(string $method, string $uri, string $controller, string $function): void
     {
         $this->routes[] = [
             'method' => $method,
@@ -14,39 +14,40 @@ class Router
             'function' => $function,
         ];
     }
- 
-    public function run()
+    public function run()  
     {
         $method = $_SERVER['REQUEST_METHOD'];
-        $uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+        if ($method === 'POST' && isset($_POST['_method'])) {
+            $method = strtoupper($_POST['_method']);
+        }
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
  
-        foreach ($this->routes as $route){
+        foreach ($this->routes as $route) {
             $pattern = str_replace(
-            '{id}',
-            '([0-9]+)',
-            $route['uri']
+                '{id}',
+                '([0-9]+)',
+                $route['uri']
             );
             $pattern = '#^' . $pattern . '$#';
-            // /students/{id}-> /students/#^([0-9]+)$# = /students/1
  
-            if(preg_match($pattern, $uri, $matches)){
+            if (preg_match($pattern, $uri, $matches) && $method === $route['method']) {
                 array_shift($matches);
  
-                require_once '../app/controllers/'. $route['controller'] . '.php';
+ 
+                require_once '../app/controllers/' . $route['controller'] . '.php';
  
                 $controllerClass = 'App\\Controllers\\' . $route['controller'];
                 $controller = new $controllerClass();
                 $function = $route['function'];
-                call_user_func_array([$controller,$function],$matches);
+ 
+                call_user_func_array([$controller, $function], $matches);
                 return;
             }
         }
-       
  
  
-        http_response_code(404);
-         echo '<h1>404 - Page Not Found</h1>';
+   
+    http_response_code(404);
+    echo "<h1>404 - Page Not Found</h1>";
     }
 }
- 
-?>
